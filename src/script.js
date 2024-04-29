9116619002d06d57e67e13f08650d10b0d9fb52e
Alune62 <seydina.alioune9319@gmail.com>
2024-04-29 13:41:47 +0200

    config && creation interface

---
+++ src/script.js
@@ -0,0 +1,8 @@
+ const btn = document.querySelector('#skills')
+
+ btn.addEventListener('click', pageskills)
+function pageskills(){
+    window.location.href = './skills.html';
+}
+
+ btn.innerHTML = `<p>bonjour</p>`
