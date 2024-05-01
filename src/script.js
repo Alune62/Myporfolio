7a25c27adf12f3f876db1d369a5c31c43d61f1a0
Alune62 <seydina.alioune9319@gmail.com>
2024-05-01 16:54:10 +0200

    config pages

--- src/script.js
+++ src/script.js
@@ -1,8 +1,37 @@
- const btn = document.querySelector('#skills')
+const weather = document.querySelector('#weather')
+weather.addEventListener('click', function() {
+    window.location.href = `https://weather-app-front-five.vercel.app/`
+})
 
- btn.addEventListener('click', pageskills)
-function pageskills(){
-    window.location.href = './skills.html';
-}
 
- btn.innerHTML = `<p>bonjour</p>`
+const news = document.querySelector('#morning')
+news.addEventListener('click', function () {
+    window.location.href =`https://morningnews-front-seven.vercel.app/`
+})
+
+
+const stay = document.querySelector('#staynomad')
+stay.addEventListener('click', function(){
+    window.location.href =`https://1drv.ms/p/s!AgZnD29zpG3Cj8FLdcGQAjfGm0qO9w?e=Ongefo&nav=eyJzSWQiOjI1NiwiY0lkIjowfQ
+    `
+})
+
+const moviz = document.querySelector('#moviz')
+moviz.addEventListener('click', function() {
+    window.location.href =`https://moviz-front-nine.vercel.app/`
+})
+
+
+const mails = document.querySelector('#mails')
+mails.addEventListener('click', function() {
+    window.location.href ='https://mymail-app.vercel.app/'
+})
+
+
+
+const pokedex = document.querySelector('#pokedex')
+console.log(pokedex);
+
+pokedex.addEventListener('click', function () {
+    window.location.href='https://pokedex-psi-three-47.vercel.app/'
+})
