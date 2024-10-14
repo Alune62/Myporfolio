8b8c18766bff7ed106363820e576a7760471034e
Alioune Mane <seydina.alioune9319@gmail.com>
2024-10-14 19:44:58 +0200

    modification cv

--- src/cvScript.js
+++ src/cvScript.js
@@ -1,6 +1,6 @@
 const cv = document.querySelector('#resume');
 cv.addEventListener('click', function() {
-    window.location.href ='./src/Resume/ALIOUNE_CV.pdf';
+    window.location.href ='./src/Resume/Alioune_DigitalHouse.pdf';
 });
 
 
